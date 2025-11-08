492f98b6a316669cefa1fd952e7d0a0aae15cf8e
Max <kirilukm157@gmail.com>
2025-11-08 21:53:35 +0200

    first commit

---
+++ IPrintable.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+/**
+ * @brief Абстрактний інтерфейс (чисто віртуальний клас) для забезпечення
+ * поліморфного виводу інформації у консоль.
+ */
+class IPrintable
+{
+public:
+    /**
+     * @brief Чиста віртуальна функція для виводу інформації об'єкта.
+     */
+    virtual void DisplayInfo() const = 0;
+
+    /**
+     * @brief Віртуальний деструктор.
+     */
+    virtual ~IPrintable() = default;
+};
