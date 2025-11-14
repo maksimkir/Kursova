681cff462170c96f6e1d58e5d4711fc47f83d382
Max <kirilukm157@gmail.com>
2025-11-14 16:20:32 +0200

    create Product.h

--- Product.h
+++ Product.h
@@ -13,13 +13,13 @@ public:
     virtual void SetName(const std::string& name) = 0;
     virtual double GetPrice() const = 0;
     virtual void SetPrice(double price) = 0;
-
+    
     /**
      * @brief Форматує дані об'єкта у рядок CSV для збереження у файл.
      */
     virtual std::string ToCSVString() const = 0;
 
     virtual void DisplayInfo() const override = 0;
-
+    
     virtual ~Product() = default;
 };
