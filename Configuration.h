a3ad8dc5c0d92edb5aeb27577305334b9b5fd924
Max <kirilukm157@gmail.com>
2025-11-19 18:10:41 +0200

    added update for Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -5,24 +5,16 @@
 #include <limits>
 #include "Logger.h"
 
-/**
- * @brief Клас Configuration. Зберігає глобальні константи та загальні утиліти.
- */
 class Configuration final
 {
 public:
-    // == Статичні константи ==
     static const std::string PRODUCT_DATA_FILE; 
     static const std::string USER_DATA_FILE;
     static Logger* s_logger;
 
-    // == Статичні утиліти для покращення інтерфейсу та надійності (3.1, 3.2) ==
+    //Статичні утиліти
     static void ClearInputBuffer();
 
-    /**
-     * @brief Забезпечує безпечний ввід числового значення.
-     * Реалізує необхідну обробку виключень через try-catch та перевірку некоректних введень.
-     */
     template <typename T>
     static T GetSafeNumberInput(const std::string& prompt);
 
