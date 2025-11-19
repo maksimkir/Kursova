5d75a8220f08d13c6dcf8c6ca5e4eda59a2f9899
Max <kirilukm157@gmail.com>
2025-11-19 18:14:25 +0200

    added update for User.cpp

--- User.cpp
+++ User.cpp
@@ -1,9 +1,7 @@
 #include "User.h"
 
-// Реалізація конструктора
+
 User::User(const std::string& login, const std::string& password)
     : m_login(login), m_password(password), m_isAuthenticated(false) 
 {
-    // Тіло конструктора залишається простим, оскільки це абстрактний клас.
 }
-// Деструктор віртуальний, але тіло не потрібне (використовуємо = default)
