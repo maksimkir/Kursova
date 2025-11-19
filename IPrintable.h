07c72b6983503bcc08b3d0c33e333d8d164d80a1
Max <kirilukm157@gmail.com>
2025-11-19 18:11:20 +0200

    added update for IPrintable.h

--- IPrintable.h
+++ IPrintable.h
@@ -2,21 +2,11 @@
 
 #include <iostream>
 
-/**
- * @brief Абстрактний інтерфейс (чисто віртуальний клас) для забезпечення
- * поліморфного виводу інформації у консоль.
- */
 class IPrintable
 {
 
 public:
-    /**
-     * @brief Чиста віртуальна функція для виводу інформації об'єкта.
-     */
     virtual void DisplayInfo() const = 0;
 
-    /**
-     * @brief Віртуальний деструктор.
-     */
     virtual ~IPrintable() = default;
 };
