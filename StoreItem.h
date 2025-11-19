950b4a5cff670668c74aa20590a92954357f767d
Max <kirilukm157@gmail.com>
2025-11-19 18:13:48 +0200

    added update for StoreItem.h

--- StoreItem.h
+++ StoreItem.h
@@ -2,20 +2,15 @@
 
 #include "Product.h"
 
-/**
- * @brief Конкретна реалізація товару магазину (StoreItem).
- * База наявності товарів: найменування, одиниця виміру, ціна одиниці, кількість, дата останнього завозу.
- */
 class StoreItem final : public Product
 {
 public:
     // Конструктори та Деструктор
     StoreItem();
     StoreItem(const std::string& name, const std::string& unit, double price, int quantity, const std::string& lastSupplyDate);
-    StoreItem(const StoreItem& other); // Копіювальний конструктор
+    StoreItem(const StoreItem& other);
     ~StoreItem();
 
-    // == Get/Set методи (PascalCase) ==
     std::string GetName() const override { return m_name; }
     void SetName(const std::string& name) override { m_name = name; }
     std::string GetUnit() const { return m_unit; }
@@ -28,12 +23,11 @@ public:
     std::string GetLastSupplyDate() const { return m_lastSupplyDate; }
     void SetLastSupplyDate(const std::string& date) { m_lastSupplyDate = date; }
 
-    // == Реалізація віртуальних методів ==
     void DisplayInfo() const override;
     std::string ToCSVString() const override;
 
 private:
-    // Поля (camelCase)
+
     std::string m_name;
     std::string m_unit;
     double m_price;
