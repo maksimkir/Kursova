789979e0dabe0107d283cfd0cbc6325f675d847c
Max <kirilukm157@gmail.com>
2025-11-21 23:18:21 +0200

    added update

--- AuthManager.h
+++ AuthManager.h
@@ -1,24 +1,20 @@
 #pragma once
 
-#include "User.h" // Тепер це лише абстрактний клас User
+#include "User.h"
 #include <vector>
 #include <memory>
 #include <string>
 
-/**
- * @brief Клас AuthManager. Керує колекцією користувачів та їхньою авторизацією.
- */
+
 class AuthManager final
 {
 public:
     AuthManager(const std::string& userFile);
     ~AuthManager();
 
-    //авторизації та вихід
     bool AuthenticateUser(const std::string& login, const std::string& password);
     void Logout() { m_currentUser.reset(); }
 
-    //адміністратор (Керування користувачами)
     void AddNewUser();
     void DeleteUser();
     void DisplayAllUsers() const;
@@ -31,12 +27,9 @@ private:
     std::unique_ptr<User> m_currentUser;
     const std::string m_userFile;
 
-    //.
-    // 4. Робота з файлами
-    bool LoadUsersFromFile(); // 4.1. Програма зчитує дані з файлів при запуску.
-    void SaveUsers() const; // 4.2. При виході — зберігає оновлені дані.
-    //.
+    bool LoadUsersFromFile();
+    void SaveUsers() const;
 
-    //обробка помилок
+
     bool ValidateUserCredentials(const std::string& login, const std::string& password) const;
 };
